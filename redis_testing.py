05e372d071bc4499343fe99fb2cc1482dd8361fe
William Jacobsen <71521465+Williamjacobsen@users.noreply.github.com>
2025-07-17 22:47:44 +0200

    .

--- redis_testing.py
+++ redis_testing.py
@@ -8,14 +8,7 @@ def pub_prompt():
     client.redis_publisher("prompt_channel", {"key": "testid", "value": "testprompt"})
 
 def pub_response():
-    client.redis_publisher(
-        "response_channel", 
-        {
-            "sessionId": "05bd7ce1-c2ea-40fe-90dd-cc7de51ef151",
-            "chatSessionName": "defualt",
-            "response": "my test response"
-        }
-    )
+    client.redis_publisher("response_channel", {"response": "my test response"})
 
 def callbackFunc(channel, data):
     print(f"Message on [{channel}]:")
@@ -23,10 +16,6 @@ def callbackFunc(channel, data):
 
 def subscribers():
     client.redis_subscriber(channels=["prompt_channel", "response_channel"], callback=callbackFunc)
-
-def simulate_bot_response():
-    time.sleep(10)
-    pub_response()
 
 if __name__ == '__main__':
     threading.Thread(target=subscribers, daemon=True).start()
